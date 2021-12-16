07329052b0bb7601a73a4062289da8e4c623fc0d
anjali <anjali.p@communicationcrafts.com>
2021-12-16 19:07:59 +0530

    add published data

--- src/FeedbackServiceProvider.php
+++ src/FeedbackServiceProvider.php
@@ -24,7 +24,14 @@ class FeedbackServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->publishes([__DIR__.'/routes/web.php' => resource_path('routes/feedback.php')], 'route');
+
         $this->loadViewsFrom(__DIR__.'/views', 'feedback');
+        $this->publishes([__DIR__.'/views' => resource_path('resources/views/feedback/')],'views');
+
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->publishes([__DIR__.'/database/migrations' => database_path('database/migrations/')], 'migration');
+
+        $this->publishes([__DIR__.'/FeedbackController' => 'app/http/controllers/'], 'controller');
     }
 }
