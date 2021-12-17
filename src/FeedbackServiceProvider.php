5033c8c1f1f1de9b9340e572315743f6d4e95525
anjali <anjali.p@communicationcrafts.com>
2021-12-17 19:35:14 +0530

    changes for publishes

--- src/FeedbackServiceProvider.php
+++ src/FeedbackServiceProvider.php
@@ -13,6 +13,7 @@ class FeedbackServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->register('Anjalicct\Feedback\FeedbackServiceProvider');
     }
 
     /**
